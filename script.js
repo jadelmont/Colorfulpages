9c38e37a2a23bf5836547a7c2a6589e448c9d728
Jasly Delmonte <jdelmont@iu.edu>
2024-11-27 17:02:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let flexContainer = document.querySelector('#flexContainer');
+let box1 = document.querySelector('#box1');
+let box2 = document.querySelector('#box2');
+let box3 = document.querySelector('#box3');
+
+
+flexContainer.style.backgroundColor = '#d3d3d3'; 
+flexContainer.style.border = '2px solid #000'; 
+
+
+box1.style.backgroundColor = '#ff6347'; 
+box1.style.border = '2px solid #000'; 
+box1.style.padding = '10px'; 
+
+box2.style.backgroundColor = '#4682b4'; 
+box2.style.border = '2px dashed #000'; 
+box2.style.transform = 'rotate(10deg)'; 
+
+box3.style.backgroundColor = '#32cd32'; 
+box3.style.border = '2px dotted #000'; 
+box3.style.opacity = '0.9'; 
